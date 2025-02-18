551dde0156e2e7eaf8ad7a5b8abdeae9aee17fed
gibrandevlab <afwangibran19@gmail.com>
2025-02-18 21:22:10 +0700

    minor changes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,16 +29,13 @@ class AuthenticatedSessionController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'login'    => 'required', // Bisa NIM atau email
+            'login'    => 'required',
             'password' => 'required',
-            // Jika menggunakan reCAPTCHA, tambahkan validasi di sini.
         ]);
 
-        // Menggabungkan input login dan IP untuk membuat key pembatasan.
         $loginInput = $request->input('login');
         $throttleKey = Str::lower($loginInput) . '|' . $request->ip();
 
-        // Cek apakah jumlah percobaan melebihi batas (misal: 5 kali)
         if (RateLimiter::tooManyAttempts($throttleKey, 5)) {
             $seconds = RateLimiter::availableIn($throttleKey);
             return back()->withErrors(['login' => "Terlalu banyak percobaan login. Silakan coba lagi dalam {$seconds} detik."]);
@@ -46,7 +43,6 @@ class AuthenticatedSessionController extends Controller
 
         $password = $request->input('password');
 
-        // Cek apakah input merupakan email atau NIM.
         $user = User::where('email', $loginInput)->first();
 
         if (!$user) {
@@ -57,25 +53,18 @@ class AuthenticatedSessionController extends Controller
         }
 
         if ($user && Hash::check($password, $user->password)) {
-            // Jika login berhasil, hapus hitungan rate limiter.
             RateLimiter::clear($throttleKey);
 
-            // Login dan regenerasi session untuk keamanan.
             Auth::login($user);
             $request->session()->regenerate();
 
             return redirect()->intended(route('dashboard'));
         }
-
-        // Tambahkan hit rate limiter jika login gagal (blokir selama 60 detik untuk setiap percobaan gagal).
         RateLimiter::hit($throttleKey, 60);
 
         return back()->withErrors(['login' => 'Login gagal, periksa kembali NIM/Email dan Password.']);
     }
 
-    /**
-     * Menghentikan sesi autentikasi.
-     */
     public function destroy(Request $request)
     {
         Auth::logout();
